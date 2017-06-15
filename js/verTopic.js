f1186e3fd8e048f711d1dc860d93771a843ac40a
KarlaNavaM <karla.nm8@gmail.com>
2017-06-15 16:56:34 -0500

    Logré sacar el id y concatenarlo en la url

--- js/verTopic.js
+++ js/verTopic.js
@@ -3,4 +3,30 @@ var topicId = getParameterByName('topic_id');
 //Solo por propositos de debug
 if(topicId){
   alert("El topic ID es:"+topicId);
+    con.log(topicId);
 }
+
+var api = {
+  url: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics/'
+};
+
+$.getJSON(api.url, function (tema) {
+    tema.forEach(urlIndividual);
+    console.log(urlIndividual);
+});
+
+var cargarPagina = function () {
+    cargarRespuestas();
+    urlIndividual();
+};
+
+var urlIndividual = function(tema) {
+    console.log(tema);
+    var id = tema.id;
+    console.log(id);
+    
+    var urlPorTema = api.url + id + "/responses";
+};
+
+
+$(document).ready(cargarPagina);
